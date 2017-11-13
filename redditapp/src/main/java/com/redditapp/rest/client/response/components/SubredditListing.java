f8b7db9760cd3f108231766033e9b6c59c3187f7
Derek Lathan <dereklathan@gmail.com>
2017-11-13 00:35:57 -0600

    subclassed Listing

--- redditapp/src/main/java/com/redditapp/rest/client/response/components/SubredditListing.java
+++ redditapp/src/main/java/com/redditapp/rest/client/response/components/SubredditListing.java
@@ -9,30 +9,12 @@ package com.redditapp.rest.client.response.components;
  *
  * @author derek
  */
-public class SubredditListing {
-    private String modhash;
-    private String whitelist_status;
+public class SubredditListing extends Listing {
+
     private SubredditListingChild[] children;
-    private String after;
-    private String before;
-    
-    public String getModHash() {
-        return modhash;
-    }
-    
-    public String getWhitelistStatus() {
-        return whitelist_status;
-    }
     
     public SubredditListingChild[] getChildren() {
         return children;
     }
     
-    public String getAfter() {
-        return after;
-    }
-    
-    public String getBefore() {
-        return before;
-    }
 }
