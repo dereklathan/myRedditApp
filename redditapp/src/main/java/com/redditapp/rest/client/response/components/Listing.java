0a35464b1ee592254199b16f8affcb5702731eec
Derek Lathan <dereklathan@gmail.com>
2017-11-13 00:35:27 -0600

    added Listing superclass

---
+++ redditapp/src/main/java/com/redditapp/rest/client/response/components/Listing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.client.response.components;
+
+/**
+ *
+ * @author derek
+ */
+public class Listing {
+    protected String modhash;
+    protected String whitelist_status;
+    protected String after;
+    protected String before;
+    
+    public String getModHash() {
+        return modhash;
+    }
+    
+    public String getWhitelistStatus() {
+        return whitelist_status;
+    }
+    
+    public String getAfter() {
+        return after;
+    }
+    
+    public String getBefore() {
+        return before;
+    }
+}
