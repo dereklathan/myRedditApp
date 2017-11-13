1ddd36e7aa5fc32284b1901c9d15704bc312ab81
Derek Lathan <dereklathan@gmail.com>
2017-11-13 16:04:41 -0600

    added LinkListingChildren

---
+++ redditapp/src/main/java/com/redditapp/rest/client/response/components/LinkListingChild.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.client.response.components;
+
+/**
+ *
+ * @author derek
+ */
+public class LinkListingChild extends ListingChild{
+    private Link data;
+    
+    public Link getLink() {
+        return data;
+    }
+}
