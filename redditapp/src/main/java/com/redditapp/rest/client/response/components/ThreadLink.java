bd594fad4a5e251d2f49dc2ea56e1a5dd614f501
Derek Lathan <dereklathan@gmail.com>
2017-11-15 03:24:58 -0600

    added ThreadLink

---
+++ redditapp/src/main/java/com/redditapp/rest/client/response/components/ThreadLink.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.client.response.components;
+
+/**
+ *
+ * @author derek
+ */
+public class ThreadLink extends Link {
+    private Float upvote_ratio;
+    
+    public Float getUpvoteRatio;
+}
