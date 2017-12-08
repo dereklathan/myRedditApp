03e0a510f8fe8af76284df16baf6a0879475fbb8
Derek Lathan <dereklathan@gmail.com>
2017-12-07 19:57:24 -0600

    singleton for storing sessiondata instances

---
+++ redditapp/src/main/java/com/redditapp/rest/resource/session/SessionPool.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.resource.session;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.Singleton;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author derek
+ */
+@Named
+@Singleton
+@ApplicationScoped
+public class SessionPool {
+    Map<String,SessionData> sessions;
+    
+    public SessionPool() {
+        sessions = new HashMap();
+    }
+    
+    public SessionData getSession(String key) {
+        SessionData session =  sessions.get(key);
+        if(session == null) {
+            return null;
+        }
+        else {
+            session.setLastAccessed(LocalDateTime.now());
+            return session;
+        }
+    }
+    
+    public String addSession(SessionData session) {
+        sessions.put(session.getAccessToken(), session);
+        return session.getAccessToken();
+    }
+    
+    public Map<String,SessionData> getAllSessions() {
+        return sessions;
+    }
+}
