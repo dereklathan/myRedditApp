bed24c7f1257f68692d99c641d55fddfdba245da
Derek Lathan <dereklathan@gmail.com>
2017-12-07 20:13:15 -0600

    adding RedditUserResponse

---
+++ redditapp/src/main/java/com/redditapp/rest/resource/response/RedditUserResponse.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.resource.response;
+
+import java.util.List;
+import com.redditapp.rest.resource.response.components.RedditUser;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author derek
+ */
+public class RedditUserResponse extends BaseResponse {
+    private List<RedditUser> redditUsers;
+    
+    public RedditUserResponse() {
+        redditUsers = new ArrayList<>();
+    }
+    
+    public void addRedditUser(int id, String username) {
+        RedditUser redditUser = new RedditUser();
+        redditUser.setId(id);
+        redditUser.setUsername(username);
+        redditUsers.add(redditUser);
+    }
+}
