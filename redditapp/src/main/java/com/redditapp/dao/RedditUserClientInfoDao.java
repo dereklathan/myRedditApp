036ea75de9e3ebeb8665507e577308c9160d99b8
Derek Lathan <dereklathan@gmail.com>
2017-12-12 11:48:52 -0600

    removed unused import. added methods with longwinded names: getRedditUserClientInfosByRedditUserId getRedditUserClientInfoAddedByIdAndClientInfoId getAuthorizedByRedditUserIdAddedBy getUnauthorizedByRedditUserIdAddedBy getUnauthorizedAddedBy

--- redditapp/src/main/java/com/redditapp/dao/RedditUserClientInfoDao.java
+++ redditapp/src/main/java/com/redditapp/dao/RedditUserClientInfoDao.java
@@ -8,7 +8,6 @@ package com.redditapp.dao;
 import com.redditapp.entity.ClientInfo;
 import com.redditapp.entity.RedditUser;
 import com.redditapp.entity.RedditUserClientInfo;
-import java.util.Collections;
 import java.util.List;
 import javax.inject.Named;
 import javax.persistence.NoResultException;
@@ -34,6 +33,19 @@ public class RedditUserClientInfoDao extends BaseDao<RedditUserClientInfo> {
         return results;
     }
     
+    public List<RedditUserClientInfo> getRedditUserClientInfosByRedditUserId(int redditUserId) {
+        List<RedditUserClientInfo> results;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query<RedditUserClientInfo> query = session
+                    .createQuery("from RedditUserClientInfo r where r.redditUser.id = :redditUserId", RedditUserClientInfo.class)
+                    .setParameter("redditUserId", redditUserId);
+            results = query.list();
+            session.getTransaction().commit();
+        }
+        return results;
+    }
+    
     public List<RedditUserClientInfo> getRedditUserClientInfosByClientInfo(ClientInfo clientInfo) {
         List<RedditUserClientInfo> results;
         try (Session session = sessionFactory.getCurrentSession()) {
@@ -42,6 +54,68 @@ public class RedditUserClientInfoDao extends BaseDao<RedditUserClientInfo> {
                     .createQuery("from RedditUserClientInfo r where r.clientInfo = :clientInfo", RedditUserClientInfo.class)
                     .setParameter("clientInfo", clientInfo);
             results = query.list();
+            session.getTransaction().commit();
+        }
+        return results;
+    }
+    
+    public RedditUserClientInfo getRedditUserClientInfoAddedByIdAndClientInfoId(int addedById, int clientInfoId) {
+        RedditUserClientInfo redditUserClientInfo = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            try {
+                Query query = session
+                        .createQuery("from RedditUserClientInfo r where r.redditUser.addedBy.id = :addedById and r.clientInfo.id = :clientInfoId")
+                        .setParameter("addedById", addedById)
+                        .setParameter("clientInfoId", clientInfoId);
+                redditUserClientInfo = (RedditUserClientInfo)query.getSingleResult();
+            }
+            catch(NoResultException ex) {
+                //redditUserClientInfo = null
+            }
+            session.getTransaction().commit();
+        }
+        return redditUserClientInfo;
+    }
+    
+    public List<RedditUserClientInfo> getAuthorizedByRedditUserIdAddedBy(int redditUserId, int addedById) {
+        List<RedditUserClientInfo> results;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from RedditUserClientInfo r where r.redditUser.id = :redditUserId "
+                            + "and r.redditUser.addedBy.id = :addedById and r.tokenInfo is not null")
+                    .setParameter("redditUserId", redditUserId)
+                    .setParameter("addedById", addedById);
+            results = query.getResultList();
+            session.getTransaction().commit();
+        }
+        return results;
+    }
+    
+    public List<RedditUserClientInfo> getUnauthorizedByRedditUserIdAddedBy(int redditUserId, int addedById) {
+        List<RedditUserClientInfo> results;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from RedditUserClientInfo r where r.redditUser.id = :redditUserId "
+                        + "and r.redditUser.addedBy.id = :addedById and r.tokenInfo is null")
+                    .setParameter("redditUserId", redditUserId)
+                    .setParameter("addedById", addedById);
+            results = query.getResultList();
+            session.getTransaction().commit();
+        }
+        return results;
+    }
+    
+    public List<RedditUserClientInfo> getUnauthorizedAddedBy(int addedById) {
+        List<RedditUserClientInfo> results;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from RedditUserClientInfo r where r.redditUser.addedBy.id = :addedById and r.tokenInfo is null")
+                    .setParameter("addedById", addedById);
+            results = query.getResultList();
             session.getTransaction().commit();
         }
         return results;
