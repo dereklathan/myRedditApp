bf183f8939d6fd4a81b90e73340a806018d94320
Derek Lathan <dereklathan@gmail.com>
2017-12-12 11:53:59 -0600

    added ClientInfo response component

---
+++ redditapp/src/main/java/com/redditapp/rest/resource/response/components/ClientInfo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.resource.response.components;
+
+/**
+ *
+ * @author derek
+ */
+public class ClientInfo {
+    private int id;
+    private String authUrl;
+    private String redirectUrl;
+    private String clientId;
+    private String clientName;
+    private boolean canDelete;
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setAuthUrl(String authUrl) {
+        this.authUrl = authUrl;
+    }
+    
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+    
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+    
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+    
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+}
