a7d97acd35510c5edc46fff6f3016b9244b9e5b7
Derek Lathan <dereklathan@gmail.com>
2017-12-12 11:51:21 -0600

    added ClientsRequest

---
+++ redditapp/src/main/java/com/redditapp/rest/resource/request/ClientsRequest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.rest.resource.request;
+
+/**
+ *
+ * @author derek
+ */
+public class ClientsRequest {
+    private int redditUserId;
+    
+    public int getRedditUserId() {
+        return redditUserId;
+    }
+}
