f8d8323b31ea55dd7c45bdd0c1c4f51719931cb9
Derek Lathan <dereklathan@gmail.com>
2017-12-12 12:01:55 -0600

    added canDelete to response in getRedditUsers. persisting new RedditUsers and only allowing 2 RedditUser with unauthorized clients. added method deleteRedditUser

--- redditapp/src/main/java/com/redditapp/rest/resource/RedditUsers.java
+++ redditapp/src/main/java/com/redditapp/rest/resource/RedditUsers.java
@@ -6,17 +6,27 @@
 package com.redditapp.rest.resource;
 
 import com.google.gson.Gson;
+import com.redditapp.crypt.AES256Util;
+import com.redditapp.crypt.EncryptedPassword;
+import com.redditapp.dao.ClientInfoDao;
+import com.redditapp.dao.RedditUserClientInfoDao;
 import com.redditapp.dao.RedditUserDao;
+import com.redditapp.dao.UserDao;
 import com.redditapp.entity.RedditUser;
+import com.redditapp.entity.RedditUserClientInfo;
+import com.redditapp.entity.User;
 import com.redditapp.rest.resource.filter.AuthFilter;
 import com.redditapp.rest.resource.request.AddRedditUserRequest;
+import com.redditapp.rest.resource.response.BaseResponse;
 import com.redditapp.rest.resource.response.RedditUserResponse;
+import com.redditapp.rest.resource.session.SessionPool;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -30,22 +40,29 @@ import javax.ws.rs.core.Response;
 public class RedditUsers extends Resource {
     
     @Inject RedditUserDao redditUserDao;
+    @Inject UserDao userDao;
+    @Inject SessionPool sessionPool;
+    @Inject AES256Util aesUtil;
+    @Inject RedditUserClientInfoDao redditUserClientInfoDao;
+    @Inject ClientInfoDao clientInfoDao;
     
     @GET
     @Path("getredditusers")
     @AuthFilter
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getRedditUsers() {
+    public Response getRedditUsers(@Context HttpHeaders headers) {
+        int userId = this.sessionPool.getSession(headers.getHeaderString("access-token")).getUserId();
         Gson gson = new Gson();
         RedditUserResponse response = new RedditUserResponse();
-        List<RedditUser> redditUsers = redditUserDao.getRedditUsers();
+        List<RedditUser> redditUsers = redditUserDao.getRedditUsersAddedById(userId);
+        RedditUserClientInfo redditUserClientInfo;
+        boolean canDelete;
         for(RedditUser redditUser : redditUsers) {
-            response.addRedditUser(redditUser.getId(), redditUser.getUsername());
+            canDelete = this.redditUserClientInfoDao.getAuthorizedByRedditUserIdAddedBy(redditUser.getId(), userId).isEmpty();
+            response.addRedditUser(redditUser.getId(), redditUser.getUsername(), canDelete);
         }
-        String redditUserJson = gson.toJson(response, RedditUserResponse.class);
-        System.out.println(redditUserJson);
-
-        return Response.ok(redditUserJson).header("Access-Control-Allow-Origin", "*").build();
+        String responseJson = gson.toJson(response, RedditUserResponse.class);
+        return Response.ok(responseJson).header("Access-Control-Allow-Origin", "*").build();
     }
     
     @POST
@@ -56,15 +73,66 @@ public class RedditUsers extends Resource {
         Gson gson = new Gson();
         AddRedditUserRequest request = gson.fromJson(json, AddRedditUserRequest.class);
         RedditUserResponse response = new RedditUserResponse();
-        RedditUser user = redditUserDao.getRedditUserByUserName(request.getUsername());
-        if(user == null) {
-            //fakeid: not persisting yet
-            response.addRedditUser(1, request.getUsername());
-        }
-        else {
+        RedditUser redditUser = redditUserDao.getRedditUserByUserName(request.getUsername());
+        String responseJson;
+        if(redditUser == null) {
+            int userId = this.sessionPool.getSession(headers.getHeaderString("access-token")).getUserId();
+            List<RedditUser> redditUsers = this.redditUserDao.getRedditUsersAddedById(userId);
+            int unauthcount = 0;
+            for (RedditUser r : redditUsers) {
+                if (this.redditUserClientInfoDao.getAuthorizedByRedditUserIdAddedBy(r.getId(), userId).isEmpty()) {
+                    unauthcount++;
+                }
+                if (unauthcount == 2) {
+                    //2 is the limit on reddit accounts with no authorized clients
+                    response.setError("Unauthorized Reddit user limit reached.");
+                    responseJson = gson.toJson(response, RedditUserResponse.class);
+                    return Response.ok(responseJson).header("Access-Control-Allow-Origin", "*").build();
+                }
+            }
+            User user = this.userDao.getUserById(userId);
+            redditUser = new RedditUser();
+            redditUser.setUsername(request.getUsername());
+            EncryptedPassword pass = this.aesUtil.encrypt(request.getPassword());
+            redditUser.setPassword(pass.getPassword());
+            redditUser.setSalt(pass.getSalt());
+            redditUser.setAddedBy(user);
+            this.redditUserDao.saveOrUpdate(redditUser);
+            response.addRedditUser(redditUser.getId(), request.getUsername(), true);
+        } else {
             response.setError("User already exists.");
         }
-        String responseJson = gson.toJson(response, RedditUserResponse.class);
+        responseJson = gson.toJson(response, RedditUserResponse.class);
+        return Response.ok(responseJson).header("Access-Control-Allow-Origin", "*").build();
+    }
+    
+    @GET
+    @Path("deletereddituser")
+    @AuthFilter
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response deleteRedditUser(@Context HttpHeaders headers, @QueryParam("id") int redditUserId) {
+        int userId = this.sessionPool.getSession(headers.getHeaderString("access-token")).getUserId();
+        RedditUser redditUser = this.redditUserDao.getRedditUserByIdAndAddedById(redditUserId, userId);
+        if(redditUser == null) {
+            return Response.serverError().status(403).header("Access-Control-Allow-Origin", "*").build();
+        }
+        Gson gson = new Gson();
+        String responseJson;
+        BaseResponse response = new BaseResponse();
+        List<RedditUserClientInfo> redditUserClientInfos = this.redditUserClientInfoDao.getRedditUserClientInfosByRedditUser(redditUser);
+        for(RedditUserClientInfo r : redditUserClientInfos) {
+            if(r.getTokenInfo() != null) {
+                response.setError("Reddit user has authorized client(s)");
+                responseJson = gson.toJson(response, BaseResponse.class);
+                return Response.ok(responseJson).header("Access-Control-Allow-Origin", "*").build();
+            }
+        }
+        for(RedditUserClientInfo r : redditUserClientInfos) {
+            this.clientInfoDao.delete(r.getClientInfo());
+            this.redditUserClientInfoDao.delete(r);
+        }
+        this.redditUserDao.delete(redditUser);
+        responseJson = gson.toJson(response, BaseResponse.class);
         return Response.ok(responseJson).header("Access-Control-Allow-Origin", "*").build();
     }
     
