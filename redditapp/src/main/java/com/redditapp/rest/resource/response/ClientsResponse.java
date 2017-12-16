d638f3a179dd265d4864b6f99668d3042deec90e
Derek Lathan <dereklathan@gmail.com>
2017-12-15 21:28:29 -0600

    added methods addClient and setRedditUsername

--- redditapp/src/main/java/com/redditapp/rest/resource/response/ClientsResponse.java
+++ redditapp/src/main/java/com/redditapp/rest/resource/response/ClientsResponse.java
@@ -6,16 +6,30 @@
 package com.redditapp.rest.resource.response;
 
 import com.redditapp.rest.resource.response.components.ClientInfo;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
  * @author derek
  */
-public class ClientsResponse {
-    List<ClientInfo> clients;
+public class ClientsResponse extends BaseResponse {
+    private List<ClientInfo> clients;
+    private String redditUserName;
+    
+    public ClientsResponse() {
+        this.clients = new ArrayList();
+    }
+    
+    public void addClient(ClientInfo client) {
+        this.clients.add(client);
+    }
     
     public void setClients(List<ClientInfo> clients) {
         this.clients = clients;
+    }
+    
+    public void setRedditUserName(String redditUserName) {
+        this.redditUserName = redditUserName;
     }
 }
