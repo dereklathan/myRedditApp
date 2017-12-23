563ab6b032399f49631a72c88b622b2c6105d62f
Derek Lathan <dereklathan@gmail.com>
2017-12-23 15:19:42 -0600

    added SubredditDao

---
+++ redditapp/src/main/java/com/redditapp/dao/SubredditDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.dao;
+
+import com.redditapp.entity.Subreddit;
+
+/**
+ *
+ * @author derek
+ */
+public class SubredditDao extends BaseDao<Subreddit> {
+    
+}
