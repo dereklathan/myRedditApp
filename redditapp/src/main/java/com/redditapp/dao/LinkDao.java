1b19b163074a4f174ecae4353b7f1278e4f395ab
Derek Lathan <dereklathan@gmail.com>
2017-12-23 15:19:23 -0600

    added LinkDao

---
+++ redditapp/src/main/java/com/redditapp/dao/LinkDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redditapp.dao;
+
+import com.redditapp.entity.Link;
+
+/**
+ *
+ * @author derek
+ */
+public class LinkDao extends BaseDao<Link> {
+    
+}
