79540686184f63f8b6abb9aeed5c38b0177617f5
Derek Lathan <dereklathan@gmail.com>
2018-02-21 07:06:38 -0600

    added methods getByRedditUserIdAndLinkId, getByRedditUserIdAndThingId, getByAddedByIdAndThingId, and getByThingId

--- redditapp/src/main/java/com/redditapp/dao/VoteLinkDao.java
+++ redditapp/src/main/java/com/redditapp/dao/VoteLinkDao.java
@@ -6,6 +6,10 @@
 package com.redditapp.dao;
 
 import com.redditapp.entity.VoteLink;
+import java.util.List;
+import javax.persistence.NoResultException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 /**
  *
@@ -13,4 +17,68 @@ import com.redditapp.entity.VoteLink;
  */
 public class VoteLinkDao extends BaseDao<VoteLink> {
     
+    public VoteLink getByRedditUserIdAndLinkId(int redditUserId, int linkId) {
+        VoteLink voteLink = null;
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from VoteLink v where v.redditUser.id = :redditUserId and v.link.id = :linkId", VoteLink.class)
+                    .setParameter("redditUserId", redditUserId)
+                    .setParameter("linkId", linkId);
+            try {
+                voteLink = (VoteLink)query.getSingleResult();
+            }
+            catch(NoResultException ex) {
+                //voteLink = null;
+            }
+            session.getTransaction().commit();
+        }
+        return voteLink;
+    }
+    
+    public VoteLink getByRedditUserIdAndThingId(int redditUserId, String thingId) {
+        VoteLink voteLink = null;
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from VoteLink v where v.redditUser.id = :redditUserId and v.link.thingId = :thingId", VoteLink.class)
+                    .setParameter("redditUserId", redditUserId)
+                    .setParameter("thingId", thingId);
+            try {
+                voteLink = (VoteLink)query.getSingleResult();
+            }
+            catch(NoResultException ex) {
+                //voteLink = null;
+            }
+            session.getTransaction().commit();
+        }
+        return voteLink;
+    }
+    
+    public List<VoteLink> getByAddedByIdAndThingId(int userId, String thingId) {
+        List<VoteLink> results;
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from VoteLink v where v.redditUser.addedBy.id = :userId and v.link.thingId = :thingId", VoteLink.class)
+                    .setParameter("userId", userId)
+                    .setParameter("thingId", thingId);
+            results = query.list();
+            session.getTransaction().commit();
+        }
+        return results;
+    }
+    
+    public List<VoteLink> getByThingId(String thingId) {
+        List<VoteLink> results;
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from VoteLink v where v.link.thingId = :thingId", VoteLink.class)
+                    .setParameter("thingId", thingId);
+            results = query.list();
+            session.getTransaction().commit();
+        }
+        return results;
+    }
 }
